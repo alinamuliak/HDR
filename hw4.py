46dfbcf3ae6f5011de0fbd8f78e074d474136f3e
alinamuliak <alina.muliak@ucu.edu.ua>
2022-07-08 11:17:53 +0100

    changed writing photos from gray to rgb

--- hw4.py
+++ hw4.py
@@ -1,24 +1,23 @@
 import cv2
 from time import sleep
-
 import numpy as np
 
 if __name__ == '__main__':
     # -1 -> 0.64 seconds shutter speed; -13 -> 0.00015 seconds shutter speed
-    exposures = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13]
+    exposures = [i for i in range(-1, -14, -1)]
     suitable_exposure_index = -1
     current_exposure_index = 0  # starting from the largest (so that the first is the brightest)
     cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
     cam.set(cv2.CAP_PROP_EXPOSURE, exposures[current_exposure_index])
     cv2.namedWindow("camera")
 
     # img_counter = 0
     while True:
         ret, frame = cam.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         if not ret:
             print("failed to grab frame")
-            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow("camera", gray)
 
         k = cv2.waitKey(1)
@@ -28,7 +27,6 @@ if __name__ == '__main__':
             break
         elif k % 256 == 32:  # SPACE pressed
             while True:
-                sleep(1)
                 ret, frame = cam.read()
                 if not ret:
                     print("failed to grab frame")
@@ -36,9 +34,9 @@ if __name__ == '__main__':
                 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 cv2.imshow("camera", gray)
                 if current_exposure_index == 0:
-                    if np.count_nonzero(gray >= 255) < len(gray) * 0.2:
+                    if np.count_nonzero(gray >= 254) < len(gray) * 0.2:
                         img_name = f'underexposed{exposures[0]}.png'
-                        cv2.imwrite(img_name, gray)
+                        cv2.imwrite(img_name, frame)
                         print(
                             f"THE SCENE IS TOO DARK, saved frame to {img_name} with exposure value {exposures[0]}")
                         cam.release()
@@ -48,8 +46,8 @@ if __name__ == '__main__':
                 if np.amax(gray) >= 255:
 
                     if current_exposure_index == len(exposures) - 1:
-                        img_name = f'overexposed.png'
-                        cv2.imwrite(img_name, gray)
+                        img_name = f'overexposed{exposures[current_exposure_index]}.png'
+                        cv2.imwrite(img_name, frame)
                         print(f"THE SCENE IS TOO BRIGHT, saved frame to {img_name} with exposure value {exposures[current_exposure_index]}")
                         cam.release()
                         cv2.destroyAllWindows()
@@ -61,9 +59,10 @@ if __name__ == '__main__':
                 else:
                     suitable_exposure_index = current_exposure_index
                     img_name = f'gray{exposures[current_exposure_index]}.png'
-                    cv2.imwrite(img_name, gray)
+                    cv2.imwrite(img_name, frame)
                     print(f"{img_name} written!")
                     print(f"FOUNDED STARTING EXPOSURE {exposures[suitable_exposure_index]}")
+                    cv2.imshow("camera", gray)
                     break
                 cv2.imshow("camera", gray)
 
@@ -79,7 +78,7 @@ if __name__ == '__main__':
                 cam.set(cv2.CAP_PROP_EXPOSURE, exposures[current_exposure_index])
                 sleep(2)
                 img_name = f'gray{exposures[suitable_exposure_index]}.png'
-                cv2.imwrite(img_name, gray)
+                cv2.imwrite(img_name, frame)
                 print(f"{img_name} written with {exposures[suitable_exposure_index]} exposure!")
 
     cam.release()
